b35f0ba0ab2b8e43a04d0bf301c04c14701755b9
gngrninja <thegngrninja@gmail.com>
2017-07-24 15:47:36 -0700

    Adding update for rlstats api

--- Modules/RocketLeague/RlStatsApi.cs
+++ Modules/RocketLeague/RlStatsApi.cs
@@ -4,16 +4,36 @@ using System.Net.Http.Headers;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using NinjaBotCore.Models.RocketLeague;
+using System.Threading.Tasks;
 
 namespace NinjaBotCore.Modules.RocketLeague
 {
     public class RlStatsApi
     {
+        private static List<Platforms> _platforms = null;
+
+        public RlStatsApi()
+        {
+            Platforms = GetCurrentPlatforms();
+        }
+
+        public static List<Platforms> Platforms 
+        {
+            get 
+            {
+                return _platforms;
+            }
+            set 
+            {
+                _platforms = value;
+            }
+        }
+
         public string RlStatsApiRequest(string url)
         {
             string response = string.Empty;
             string rlStatsKey = $"{Config.RlStatsApi}";
-            string baseUrl = "https://api.rocketleaguestats.com/v1/data";
+            string baseUrl = "https://api.rocketleaguestats.com/v1";
 
             url = $"{baseUrl}{url}";
             Console.WriteLine($"Calling RlStats API with URL: {url}");
@@ -33,9 +53,17 @@ namespace NinjaBotCore.Modules.RocketLeague
         public List<Platforms> GetCurrentPlatforms()
         {
             List<Platforms> p = null;
-            string url = "/platforms";
+            string url = "/data/platforms";
             p = JsonConvert.DeserializeObject<List<Platforms>>(RlStatsApiRequest(url));
             return p;
+        }
+
+        public async Task<UserStats> GetUserStats(long uniqueId, int platformId = 1)
+        {
+            UserStats s = null;                        
+            string url = $"/player?unique_id={uniqueId}&platform_id={platformId}";
+            s = JsonConvert.DeserializeObject<UserStats>(RlStatsApiRequest(url));
+            return s;
         }        
     }
 }
