7ab031426286a9de980ed21be36294146057914c
gngrninja <thegngrninja@gmail.com>
2025-12-21 19:02:05 -0800

    adding migrations

---
+++ src/Migrations/20251221191348_wowguild.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace NinjaBotCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class wowguild : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WowGuildRosterMembers",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    GuildName = table.Column<string>(type: "text", nullable: true),
+                    RealmSlug = table.Column<string>(type: "text", nullable: true),
+                    GuildRealmSlug = table.Column<string>(type: "text", nullable: true),
+                    Faction = table.Column<string>(type: "text", nullable: true),
+                    Region = table.Column<string>(type: "text", nullable: true),
+                    CharacterName = table.Column<string>(type: "text", nullable: true),
+                    Level = table.Column<int>(type: "integer", nullable: false),
+                    Rank = table.Column<int>(type: "integer", nullable: false),
+                    LastUpdated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WowGuildRosterMembers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WowGuildRosterMembers");
+        }
+    }
+}
