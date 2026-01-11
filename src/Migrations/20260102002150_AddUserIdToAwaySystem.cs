e0255a306c340e351164bee6e0211f1f6f49a391
thegnja <thegngrninja@gmail.com>
2026-01-10 23:00:34 -0800

    migration fixes

--- src/Migrations/20260102002150_AddUserIdToAwaySystem.cs
+++ src/Migrations/20260102002150_AddUserIdToAwaySystem.cs
@@ -12,7 +12,15 @@ namespace NinjaBotCore.Migrations
         {
             // Clear existing away statuses since we can't reliably map UserName to UserId
             // Users will need to re-set their away status after this migration
-            migrationBuilder.Sql("DELETE FROM \"AwaySystem\"");
+            // Use TRUNCATE for fresh installs (empty table) and DELETE for existing data
+            migrationBuilder.Sql(@"
+                DO $$
+                BEGIN
+                    IF EXISTS (SELECT FROM information_schema.tables WHERE table_name = 'AwaySystem') THEN
+                        DELETE FROM ""AwaySystem"";
+                    END IF;
+                END $$;
+            ");
 
             migrationBuilder.AddColumn<decimal>(
                 name: "UserId",
